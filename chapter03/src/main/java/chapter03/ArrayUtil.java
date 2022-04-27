f034f65fa2490f8a703d37aacbef97a15e55c1e1
chaewon <rlacodnjs98@naver.com>
2022-04-27 16:27:39 +0900

    클래스 정의: method #finish

---
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -0,0 +1,24 @@
+package chapter03;
+
+public class ArrayUtil {
+	public static double[] intToDouble(int[] a) {
+		double[] result = new double[a.length];
+		for (int i = 0; i < a.length; i++) {
+			result[i] = a[i];
+			
+		}
+		return result;
+	}
+
+	public static double[] doubleToInt(double[] ds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static double[] concat(int[] is, int[] is2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
