f3303a5665cdd803fac9bd7793492c2742620e85
chaewon <rlacodnjs98@naver.com>
2022-05-06 18:21:28 +0900

    NSLookup 과제

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -1,10 +1,38 @@
 package util;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class NSLookup {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = null;
+		while(true) {
+			try {
+				scanner = new Scanner(System.in);
 
+
+				System.out.print(">");
+				String line = scanner.nextLine();
+
+				if("exit".equals(line)) {
+					scanner.close();
+					break;
+				}
+
+				InetAddress[] inetAddresses = InetAddress.getAllByName(line);
+//				System.out.println(inetAddresses.length);
+				for (InetAddress inetAddress : inetAddresses) {
+					System.out.println(inetAddress.getHostName() + " : " + inetAddress.getHostAddress());
+				}
+
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} 
+
+			
+		}
 	}
-
 }
+	
