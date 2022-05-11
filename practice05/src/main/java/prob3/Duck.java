72dd1fb225aa44057337b1c5ac4e30fd262ec377
chaewon <rlacodnjs98@naver.com>
2022-05-11 16:38:46 +0900

    practice05 setting

---
+++ practice05/src/main/java/prob3/Duck.java
@@ -0,0 +1,17 @@
+package prob3;
+
+public class Duck extends Bird {
+
+	@Override
+	protected void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void sing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ practice05/src/main/java/prob3/Sparrow.java
@@ -0,0 +1,17 @@
+package prob3;
+
+public class Sparrow extends Bird {
+
+	@Override
+	protected void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void sing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
